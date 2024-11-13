3354ccd1349299f5dba28e2b6ecdb2bd015b1da7
Karan <karankhandekar028@gmail.com>
2024-11-13 18:22:45 +0530

    efficiently rendered the list items on the screen using ListView and itemBuilder

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -26,10 +26,11 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(children: [
-        Text('The chart'),
-        Text('Expenses list...'),
-      ]),
+      body: Column(
+        children: [
+          Text('The chart'),
+        ],
+      ),
     );
   }
 }
